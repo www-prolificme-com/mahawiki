316429de1bdd591e783f34eb12c9f9bbd90c958d
Ankit Tiwari <30570096+ankitt12@users.noreply.github.com>
2019-02-27 21:05:45 +0530

    Changed initialization of max variable

--- Python Challenge/Solution3/solution_three_niket - niket doke.py
+++ Python Challenge/Solution3/solution_three_niket - niket doke.py
@@ -10,7 +10,9 @@ def find_most_occurring(string):
         else:
             hash[c] = 1
     max = hash[string[0]]
-
+    # if max is initialize to hash[string[0]] in case input is all characters appearing only once like "a b c d" 
+    #output will be empty 
+    # so initialize max to some min value like count can never be negative so initialize max = -1 or 0
     keys = hash.keys()
     for key in keys:
         if ( hash[key] > max):
